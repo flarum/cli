2e801176da0a46762d9c546a764b7be5e99529ab
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-03-16 02:18:32 -0400

    Cleanup

--- src/commands/update/js-imports.ts
+++ src/commands/update/js-imports.ts
@@ -1,13 +1,16 @@
+import cli from 'cli-ux';
 import glob from 'glob';
 import BaseFsCommand from '../../util/BaseFsCommand';
 import { MemFsUtil } from '../../util/MemfsUtil';
 
+const CORE_JS_NAMESPACES = ['admin', 'common', 'forum'];
+const CORE_NAMESPACE_REGEX = new RegExp(`^(${CORE_JS_NAMESPACES.join('|')})/`);
 const IMPORT_REGEX = /(?<key>import|export)\s+(?:(?<alias>[\w,{}\s\*]+)\s+from)?\s*(?:(["'])?(?<ref>[@\w\s\\\/.-]+)\3?)\s*;?/mg;
 
 type ImportMap = { [name: string]: string };
 
 export default class UpdateJsImports extends BaseFsCommand {
-  static description = 'add/update backend testing infrastructure'
+  static description = 'updates JS imports from core to use proper namespaces'
 
   static flags = { ...BaseFsCommand.flags };
 
@@ -40,7 +43,7 @@ export default class UpdateJsImports extends BaseFsCommand {
     glob.sync(`${jsSrcDir}**/*.{js,jsx,ts,tsx}`)
       .forEach((currPath) => {
         const withNamespace = currPath.slice(jsSrcDir.length).replace(/\.(js|jsx|ts|tsx)$/, '');
-        const noNamespace = withNamespace.replace(/^(admin|common|forum)\//, '');
+        const noNamespace = withNamespace.replace(CORE_NAMESPACE_REGEX, '');
 
         importMap[`flarum/${noNamespace}`] = `flarum/${withNamespace}`;
       });
@@ -49,18 +52,37 @@ export default class UpdateJsImports extends BaseFsCommand {
   }
 
   async updateJsFiles(dir: string, importMap: ImportMap) {
+    cli.action.start('Updating imports');
+
+    let files = 0;
+    let importsChanged = 0;
+
     glob.sync(dir + '/js/src/**/*.{js,jsx,ts,tsx}')
       .forEach(match => {
+        let fileCounted = false;
         const currContents = this.fs.read(match);
         const newContents = currContents.replace(IMPORT_REGEX, (match, ...args) => {
+
           const currImport = args[3] as string;
           const currImportNoAt = currImport.replace(/^@/, '');
           const newImport = importMap[currImportNoAt] || currImport;
+
+          if (newImport !== currImport) {
+            importsChanged++;
+            if (!fileCounted) {
+              fileCounted = true;
+              files++;
+            }
+          }
 
           return match.replace(currImport, newImport);
         });
 
         this.fs.write(match, newContents);
       });
+
+    cli.action.stop();
+    this.log(`Updated ${importsChanged} imports in ${files} files.`);
+    this.log("Please make sure to check my work and test before commiting!!!");
   }
 }
