792815f96cfd9cbc83d616cb6187dde4bdad9630
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-03-18 03:04:19 -0400

    PHP Subsystem: vendor

---
+++ php-subsystem/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '64cb35d4fb6f667d371cec13eb120f88dd38c180',
+    'name' => 'flarum/cli-php-util',
+  ),
+  'versions' => 
+  array (
+    'flarum/cli-php-util' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '64cb35d4fb6f667d371cec13eb120f88dd38c180',
+    ),
+    'nikic/php-parser' => 
+    array (
+      'pretty_version' => 'v4.10.4',
+      'version' => '4.10.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6d052fc58cb876152f89f532b95a8d7907e7f0e',
+    ),
+  ),
+);
