f794c070ff84c142175cbb32d2cd11054c1d5c8a
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-03-19 13:19:53 -0400

    Add colors!

--- src/help.ts
+++ src/help.ts
@@ -1,26 +1,42 @@
 import { Command, Topic } from '@oclif/config';
 import { Help as HelpDefault } from '@oclif/plugin-help';
+import chalk from 'chalk';
 
 const COLON_REGEX = /:/g;
 
+function prepCommand(command: Command) {
+  return Object.assign({}, command, { id: chalk.green(command.id.replace(COLON_REGEX, " ")) });
+}
+
+function prepTopic(topic: Topic) {
+  return Object.assign({}, topic, { name: chalk.green(topic.name.replace(COLON_REGEX, " ")) });
+}
+
+function colorHeaders(text: string) {
+  return text.replace(/(TOPICS|TOPIC|DESCRIPTION|USAGE|COMMANDS|VERSION|ARGUMENTS|OPTIONS)/g, match => {
+    return chalk.yellow(match);
+  })
+}
+
+
 export default class Help extends HelpDefault {
+  protected formatRoot(): string {
+    return colorHeaders(super.formatRoot());
+  }
+
   protected formatCommand(command: Command): string {
-    return super.formatCommand(Object.assign({}, command, { id: command.id.replace(COLON_REGEX, " ") }));
+    return colorHeaders(super.formatCommand(prepCommand(command)));
   }
 
   protected formatCommands(commands: Command[]): string {
-    const spaceSeparated = commands.map(c => Object.assign({}, c, { id: c.id.replace(COLON_REGEX, " ") }));
-
-    return super.formatCommands(spaceSeparated);
+    return colorHeaders(super.formatCommands(commands.map(prepCommand)));
   }
 
   protected formatTopic(topic: Topic): string {
-    return super.formatTopic(topic).replace(COLON_REGEX, " ");
+    return colorHeaders(super.formatTopic(prepTopic(topic)).replace(COLON_REGEX, " "));
   }
 
   protected formatTopics(topics: Topic[]): string {
-    const spaceSeparated = topics.map(t => Object.assign({}, t, { name: t.name.replace(COLON_REGEX, " ") }));
-
-    return super.formatTopics(spaceSeparated);
+    return colorHeaders(super.formatTopics(topics.map(prepTopic)));
   }
 }
