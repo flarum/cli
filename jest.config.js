6f389db6d1b391b081b539b97c7d0766dd99263a
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-05-21 00:08:34 -0400

    Switch to jest, update eslint config

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  collectCoverage: true,
+  preset: 'ts-jest',
+  coverageReporters: [
+    'html',
+    'lcov',
+    'text-summary'
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+  },
+};
