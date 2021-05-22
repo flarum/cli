58b2bf69bd846fb4c56ef90e5c963e33bb2a8efb
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-05-21 21:37:05 -0400

    Drop coverage thresholds for now

--- jest.config.js
+++ jest.config.js
@@ -6,14 +6,6 @@ module.exports = {
     'lcov',
     'text-summary',
   ],
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80,
-    },
-  },
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
     '^.+\\.(js|jsx)$': 'babel-jest',
