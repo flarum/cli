520c2e06254fb6fe81f9d0f12c04d1f2e021a356
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-05-21 21:40:24 -0400

    Add ts-ignore

--- test/utils.ts
+++ test/utils.ts
@@ -16,9 +16,9 @@ export async function runStep(StepClass: any, params: Record<string, unknown>): 
 }
 
 export function getFsPaths(store: Store, extDir = '/ext'): string[] {
-  return store
-    .all()
-    .filter(f => f.state !== 'deleted')
+  // TODO: update when issues with mem-fs typings resolved.
+  // @ts-ignore
+  return store.all().filter(f => f.state !== 'deleted')
     .map(f => f.path)
     .filter((path: string) => path.startsWith(extDir))
     .sort();
