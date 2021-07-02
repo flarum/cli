3af8f87495d892ac7082874916e052d2205f824e
SychO9 <sychocouldy@gmail.com>
2021-07-02 12:22:03 +0100

    Fix php indentation

--- boilerplate/stubs/backend/event-listener.php
+++ boilerplate/stubs/backend/event-listener.php
@@ -6,9 +6,9 @@ use \<%= eventClass %>;
 
 class <%= className %>
 {
-  public function handle(<%= eventClassName %> $event)
-  {
-    // Add logic to handle the event here.
-    // See https://docs.flarum.org/extend/backend-events.html for more information.
-  }
+    public function handle(<%= eventClassName %> $event)
+    {
+        // Add logic to handle the event here.
+        // See https://docs.flarum.org/extend/backend-events.html for more information.
+    }
 }
