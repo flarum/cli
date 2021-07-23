ae737be13e1528cf12e38f92c60c86d8fe2104fa
SychO9 <sychocouldy@gmail.com>
2021-07-23 14:02:27 +0100

    Add todo on extender formatting

--- php-subsystem/src/CustomPrettyPrinter.php
+++ php-subsystem/src/CustomPrettyPrinter.php
@@ -7,8 +7,11 @@ use PhpParser\PrettyPrinter\Standard;
 
 class CustomPrettyPrinter extends Standard
 {
+    /**
+     * @todo this is just a temporary solution, we should look into formatters such as php cs fixer
+     */
     protected function pExpr_MethodCall(Expr\MethodCall $node) {
-        return $this->pDereferenceLhs($node->var) . "\n    ->" . $this->pObjectProperty($node->name)
+        return $this->pDereferenceLhs($node->var) . "\n        ->" . $this->pObjectProperty($node->name)
              . '(' . $this->pMaybeMultiline($node->args) . ')';
     }
 }
