897c4b95b5f8b475b43992fddd36e3f14ef9ce1c
David Wheatley <hi@davwheat.dev>
2021-09-02 12:46:33 +0100

    fix: dead link in migration comment

--- boilerplate/stubs/migration.php
+++ boilerplate/stubs/migration.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
 
 // HINT: you might want to use a `Flarum\Database\Migration` helper method for simplicity!
-// See https://docs.flarum.org/extend/data.html#migrations to learn more about migrations.
+// See https://docs.flarum.org/extend/models.html#migrations to learn more about migrations.
 return [
     'up' => function (Builder $schema) {
         // up migration
