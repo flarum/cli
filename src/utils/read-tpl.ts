4bbddda36fbbdd55a34dd5096b039c6db1803c38
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-12-28 20:32:54 -0500

    Add readTpl helper function to read templates without copying to disk

---
+++ src/utils/read-tpl.ts
@@ -0,0 +1,8 @@
+import { Data, Options, render } from "ejs";
+import { readFileSync } from "fs";
+
+export function readTpl(path: string, data: Data, options?: Options): string {
+    const template = readFileSync(path, 'utf8');
+
+    return render(template, data, {filename: path, ...(options ?? {}), async: false});
+}
---
+++ test/utils/read-tpl.test.ts
@@ -0,0 +1,16 @@
+import { resolve } from "path";
+import { readTpl } from "../../src/utils/read-tpl";
+
+describe('read-tpl', function () {
+  it('errors when data missing', function() {
+    expect(() => {
+      readTpl(resolve(__dirname, '../fixtures.sample.tpl'), {})
+    }).toThrow();
+  })
+
+  it('works with data provided', function() {
+    const content = readTpl(resolve(__dirname, '../fixtures/sample.tpl'), {requiredMessage: 'Flarum is great!'});
+
+    expect(content).toStrictEqual('Hello world!\n\nFlarum is great!')
+  })
+});
