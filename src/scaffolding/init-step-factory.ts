28481d783b2e46dfdca17d9805dd29339dd14c69
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-12-30 13:56:19 -0500

    Implement init step factory

---
+++ src/scaffolding/init-step-factory.ts
@@ -0,0 +1,41 @@
+import { Store } from 'mem-fs';
+import { ParamProvider } from '../../src/provider/param-provider';
+import { PathProvider } from '../../src/provider/path-provider';
+import { PhpProvider } from '../../src/provider/php-provider';
+import { Step } from '../../src/steps/step-manager';
+import { renameKeys } from '../../src/utils/rename-keys';
+import { applyModule, Module, ModuleStatusCache, promptModulesEnabled, setModuleValues } from './module';
+import { promptParamValues, TemplateParam } from './template-param';
+
+export function initStepFactory(scaffoldDir: string, modules: Module[], templateParams: TemplateParam<unknown>[], moduleStatusCache?: ModuleStatusCache): Step {
+  let modulesEnabled: Record<string, boolean>;
+
+  return {
+    type: 'Generate package skeleton',
+
+    composable: true,
+
+    async run(fs: Store, pathProvider: PathProvider, paramProvider: ParamProvider, _phpProvider: PhpProvider): Promise<Store> {
+      const paramVals = await promptParamValues(templateParams, pathProvider, paramProvider);
+      modulesEnabled = await promptModulesEnabled(modules, paramProvider);
+
+      for (const m of modules) {
+        if (modulesEnabled[m.name]) {
+          applyModule(m, modulesEnabled, paramVals, scaffoldDir, fs, pathProvider);
+        }
+      }
+
+      if (moduleStatusCache) {
+        setModuleValues(modules, modulesEnabled, fs, pathProvider, moduleStatusCache);
+      }
+
+      return fs;
+    },
+
+    exposes: modules.map((m) => `modules.${m.name}`),
+
+    getExposed(_pathProvider: PathProvider, _paramProvider: ParamProvider): Record<string, unknown> {
+      return renameKeys(modulesEnabled, k => `modules.${k}`);
+    },
+  };
+}
---
+++ test/scaffolding/init-step-factory.test.ts
@@ -0,0 +1,115 @@
+import prompts, { prompt } from 'prompts';
+import { create as createStore } from 'mem-fs';
+
+import { paramProviderFactory } from '../../src/provider/param-provider';
+import { PathFsProvider } from '../../src/provider/path-provider';
+import { Module, ModuleStatusCache, currModulesEnabled, promptModulesEnabled, setModuleValues, applyModule } from '../../src/scaffolding/module';
+import { TemplateParam } from '../../src/scaffolding/template-param';
+import { initStepFactory } from '../../src/scaffolding/init-step-factory';
+import { resolve } from 'path';
+import { getFsPaths, runStep } from '../utils';
+
+describe('init step factory', function () {
+  const modules: Module[] = [
+    {
+      name: 'core',
+      shortDescription: 'initial core config files',
+      togglable: false,
+      updatable: true,
+      filesToReplace: ['readme.md', '.gitignore'],
+      jsonToAugment: {},
+      needsTemplateParams: ['someVar'],
+    },
+    {
+      name: 'sourceFiles',
+      shortDescription: 'Source code files',
+      togglable: true,
+      defaultEnabled: true,
+      updatable: true,
+      filesToReplace: ['src/index.html', 'src/index.js', 'src/index.ml', 'src/index.php'],
+      jsonToAugment: {'config1.json': ['nested']},
+      needsTemplateParams: ['someVar', 'someOtherVar'],
+      dependsOn: [],
+    },
+    {
+      name: 'config',
+      shortDescription: 'Config files',
+      togglable: true,
+      defaultEnabled: false,
+      updatable: true,
+      filesToReplace: ['config2.json'],
+      jsonToAugment: {'config1.json': ['hello', 'foo', 'authors']},
+      needsTemplateParams: ['someVar', 'someOtherVar'],
+      dependsOn: [],
+    },
+  ];
+
+  const templateParams: TemplateParam<unknown>[] = [
+    {
+      prompt: {
+        name: 'someVar',
+        message: 'Some Var',
+        type: 'text'
+      },
+      getCurrVal: async () => ''
+    },
+    {
+      name: 'someOtherVar',
+      uses: [],
+      compute: async (pathProvider) => pathProvider.ext(),
+    }
+  ]
+
+  const scaffoldDir = resolve(__dirname, '../fixtures/example-scaffold');
+
+  it('works if all modules enabled', async function () {
+    const step = initStepFactory(scaffoldDir, modules, templateParams);
+
+    prompts.inject(['Var Value', true, true, true]);
+
+    const {fs} = await runStep(step);
+
+    expect(getFsPaths(fs).sort()).toStrictEqual([
+      '/ext/.gitignore',
+      '/ext/config1.json',
+      '/ext/config2.json',
+      '/ext/readme.md',
+      '/ext/src/index.html',
+      '/ext/src/index.js',
+      '/ext/src/index.ml',
+      '/ext/src/index.php',
+    ])
+  });
+
+
+  it('works if all optional modules disabled', async function () {
+    const step = initStepFactory(scaffoldDir, modules, templateParams);
+
+    prompts.inject(['Var Value', true, false, false]);
+
+    const {fs} = await runStep(step);
+
+    expect(getFsPaths(fs).sort()).toStrictEqual([
+      '/ext/.gitignore',
+      '/ext/readme.md',
+    ])
+  });
+
+  it('respects module defaults if not in advanced mode', async function () {
+    const step = initStepFactory(scaffoldDir, modules, templateParams);
+
+    prompts.inject(['Var Value', false]);
+
+    const {fs} = await runStep(step);
+
+    expect(getFsPaths(fs).sort()).toStrictEqual([
+      '/ext/.gitignore',
+      '/ext/config1.json',
+      '/ext/readme.md',
+      '/ext/src/index.html',
+      '/ext/src/index.js',
+      '/ext/src/index.ml',
+      '/ext/src/index.php',
+    ])
+  });
+});
