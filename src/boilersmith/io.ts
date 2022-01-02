dcec1ad1ffa00707ce4e4162c05828c972fc1d58
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-01-02 00:05:59 -0500

    Add output support to IO interface

--- src/boilersmith/io.ts
+++ src/boilersmith/io.ts
@@ -1,20 +1,44 @@
 import prompt, { Options, PromptObject } from 'prompts';
 import { exit } from '@oclif/errors';
+import chalk from 'chalk';
 
 export type ParamDef<N extends string = string> = Omit<PromptObject<N>, 'name'> & { name: N };
 
+export type Message = { type: 'info' | 'warning' | 'error', message: string };
+
 export interface IO {
+  /**
+   * Prompt the user for some input data.
+   */
   getParam<T>(param: ParamDef): Promise<T>;
 
+  /**
+   * Check if some data has already been prompted for.
+   */
   hasCached(name: string): boolean;
 
+  /**
+   * Get all cached params.
+   */
   cached(): Record<string, unknown>;
+
+  info(message: string, immediate: boolean): void;
+  warning(message: string, immediate: boolean): void;
+
+  error(message: string, immediate: false): void;
+  error(message: string, immediate: true, exit: boolean): void;
+
+  /**
+   * Get all messages that have been logged non-immediately.
+   */
+  getOutput(): Message[];
 }
 
 export const PROMPTS_OPTIONS: Options = { onCancel: () => exit() };
 
 export class PromptsIO implements IO {
   private cache = new Map<string, unknown>();
+  private messages: Message[] = [];
 
   constructor(initial: Record<string, unknown> = {}) {
     this.cache = new Map(Object.entries(initial));
@@ -47,6 +71,42 @@ export class PromptsIO implements IO {
       obj[key] = value;
       return obj;
     }, {} as Record<string, unknown>);
+  }
+
+  info(message: string, immediate: boolean): void {
+    if (immediate) {
+      console.log(message);
+    } else {
+      this.messages.push({ type: 'info', message });
+    }
+  }
+
+  warning(message: string, immediate: boolean): void {
+    const formatted = `${chalk.yellow('Warning:')} ${message}`;
+    if (immediate) {
+      console.log(formatted);
+    } else {
+      this.messages.push({ type: 'warning', message: formatted });
+    }
+  }
+
+  error(message: string, immediate: false): void;
+  error(message: string, immediate: true, exitProcess: boolean): void;
+  error(message: string, immediate: boolean, exitProcess?: boolean): void {
+    const formatted = `${chalk.red('Error:')} ${message}`;
+    if (immediate) {
+      console.log(formatted);
+
+      if (exitProcess) {
+        exit();
+      }
+    } else {
+      this.messages.push({ type: 'warning', message: formatted });
+    }
+  }
+
+  getOutput(): Message[] {
+    return this.messages;
   }
 }
 
